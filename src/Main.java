d5844106ded776f3fab601bfefd1b790c3df0a1d
FingRinger <ard21study@gmail.com>
2021-05-24 14:52:13 +0300

    Сумма пополнения = 100 р

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,21 @@
 public class Main {
+    public static void main(String[] args) {
+
+//topupAmount - сумма пополнения счета
+        long topupAmount = 100;
+        boolean i = topupAmount > 1000;
+
+       long bonus;
+        if (i) {
+
+            bonus = topupAmount / 100;
+        }
+        else {
+             bonus = 0;
+        }
+ long currentBalance = 100;
+ long total = currentBalance + topupAmount + bonus;
+        System.out.println(total);
+        System.out.println(bonus);
+    }
 }
